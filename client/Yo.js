43f12b79280a884dba50398e24f0a3e02eaee2e5
Muhammed Abiola <hi@mabiola.net>
2022-07-07 09:31:59 +0100

    Remember to capitalise your components

---
+++ client/Yo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+const Yo = () => {
+  return (
+    <div>
+      <h1>y0!</h1>
+    </div>
+  );
+};
+
+export default hot(module)(Yo);
